0b78950514da6f5e14dc02509aaceba78683cac8
Denis Yorsh <denis_my_mail@yahoo.com>
2022-06-30 13:44:55 +0300

    implement public void clear()

--- src/main/java/com/hillel/hwYorsh2/MyLinkedListTwo.java
+++ src/main/java/com/hillel/hwYorsh2/MyLinkedListTwo.java
@@ -269,7 +269,16 @@ public class MyLinkedListTwo<E> implements List<E>, Deque<E> {
 
     @Override
     public void clear() {
-
+        for (Node<E> x = first; x != null; ) {
+            Node<E> next = x.next;
+            x.item = null;
+            x.next = null;
+            x.prev = null;
+            x = next;
+        }
+        first = last = null;
+        size = 0;
+        modCount++;
     }
 
     @Override
