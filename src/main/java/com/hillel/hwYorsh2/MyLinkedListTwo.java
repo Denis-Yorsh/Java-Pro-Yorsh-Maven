e29d8d44e3cc977449db236d00de1e239e4803e9
Denis Yorsh <denis_my_mail@yahoo.com>
2022-06-30 13:09:57 +0300

    implement public E remove(int index) private void checkElementIndex(int index) private boolean isElementIndex(int index) private String outOfBoundsMsg(int index) Node<E> node(int index)

--- src/main/java/com/hillel/hwYorsh2/MyLinkedListTwo.java
+++ src/main/java/com/hillel/hwYorsh2/MyLinkedListTwo.java
@@ -254,7 +254,8 @@ public class MyLinkedListTwo<E> implements List<E>, Deque<E> {
 
     @Override
     public E remove(int index) {
-        return null;
+        checkElementIndex(index);
+        return unlink(node(index));
     }
 
     @Override
@@ -351,6 +352,36 @@ public class MyLinkedListTwo<E> implements List<E>, Deque<E> {
         modCount++;
         return element;
     }
+
+    private void checkElementIndex(int index) {
+        if (!isElementIndex(index))
+            throw new IndexOutOfBoundsException(outOfBoundsMsg(index));
+    }
+
+    private boolean isElementIndex(int index) {
+        return index >= 0 && index < size;
+    }
+
+    private String outOfBoundsMsg(int index) {
+        return "Index: "+index+", Size: "+size;
+    }
+
+    Node<E> node(int index) {
+        // assert isElementIndex(index);
+
+        if (index < (size >> 1)) {
+            Node<E> x = first;
+            for (int i = 0; i < index; i++)
+                x = x.next;
+            return x;
+        } else {
+            Node<E> x = last;
+            for (int i = size - 1; i > index; i--)
+                x = x.prev;
+            return x;
+        }
+    }
+
     private static class Node<E> {
         E item;
         Node<E> next;
