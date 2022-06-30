215c3cf920282127117cff7a995a2574734be2ab
Denis Yorsh <denis_my_mail@yahoo.com>
2022-06-30 13:35:34 +0300

    implement public boolean removeFirstOccurrence(Object o) public boolean removeLastOccurrence(Object o)

--- src/main/java/com/hillel/hwYorsh2/MyLinkedListTwo.java
+++ src/main/java/com/hillel/hwYorsh2/MyLinkedListTwo.java
@@ -105,11 +105,26 @@ public class MyLinkedListTwo<E> implements List<E>, Deque<E> {
 
     @Override
     public boolean removeFirstOccurrence(Object o) {
-        return false;
+        return remove(o);
     }
 
     @Override
     public boolean removeLastOccurrence(Object o) {
+        if (o == null) {
+            for (Node<E> x = last; x != null; x = x.prev) {
+                if (x.item == null) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        } else {
+            for (Node<E> x = last; x != null; x = x.prev) {
+                if (o.equals(x.item)) {
+                    unlink(x);
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
