9a428461adc91d382fd989cb085dc46855bdf8fc
Denis Yorsh <denis_my_mail@yahoo.com>
2022-07-02 18:05:26 +0300

    implements private String firstName; private String lastName; private int age; public Equals(String firstName, String lastName, int age) public boolean equals(Object o) public int hashCode() public String toString()

--- src/main/java/com/hillel/hwYorsh3/Equals.java
+++ src/main/java/com/hillel/hwYorsh3/Equals.java
@@ -1,4 +1,38 @@
 package com.hillel.hwYorsh3;
 
+import java.util.Objects;
+
 public class Equals {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Equals(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Equals equals = (Equals) o;
+        return age == equals.age && Objects.equals(firstName, equals.firstName) && Objects.equals(lastName, equals.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Equals{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
