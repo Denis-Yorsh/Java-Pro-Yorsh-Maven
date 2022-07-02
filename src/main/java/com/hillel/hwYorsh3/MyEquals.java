54bb9d2f1fc88c5cacf747f77102823037cc65ab
Denis Yorsh <denis_my_mail@yahoo.com>
2022-07-02 18:31:54 +0300

    implements private boolean myEquals(Object firstObject, Object secondObject) MyEquals test

---
+++ src/main/java/com/hillel/hwYorsh3/MyEquals.java
@@ -0,0 +1,47 @@
+package com.hillel.hwYorsh3;
+
+import java.util.Objects;
+
+public class MyEquals {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public MyEquals(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Equals equals = (Equals) o;
+       // age == equals.age && Objects.equals(firstName, equals.firstName) && Objects.equals(lastName, equals.lastName);
+        return myEquals(o, firstName)
+                && myEquals(o, lastName)
+                && myEquals(o, age) ;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
+    }
+
+    @Override
+    public String toString() {
+        return "Equals{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    private boolean myEquals(Object firstObject, Object secondObject) {
+        String firstString = String.valueOf(firstObject);
+        String secondString = String.valueOf(secondObject);
+        return firstString.contains(secondString);
+    }
+}
--- src/main/java/com/hillel/hwYorsh3/TestMain.java
+++ src/main/java/com/hillel/hwYorsh3/TestMain.java
@@ -3,6 +3,7 @@ package com.hillel.hwYorsh3;
 public class TestMain {
 
     public static void main(String[] args) {
+        System.out.println("\nMyHashCode test\n");
         MyHashCode humanOne = new MyHashCode("Denis", "Yorsh", 39);
         MyHashCode humanTwo = new MyHashCode("Ivan", "Petrov", 45);
         MyHashCode humanThee = new MyHashCode("Svetlana", "Sidorova", 25);
@@ -14,5 +15,18 @@ public class TestMain {
         System.out.println("humanFour.hashCode() - " + humanFour.hashCode());
         System.out.println("humanOne.equals(humanFour) - " + humanOne.equals(humanFour));
         System.out.println("humanOne.equals(humanTwo) - " + humanOne.equals(humanTwo));
+
+        System.out.println("\nMyEquals test\n");
+        MyEquals humanFive = new MyEquals("Denis", "Yorsh", 39);
+        MyEquals humanSix = new MyEquals("Ivan", "Petrov", 45);
+        MyEquals humanSeven = new MyEquals("Svetlana", "Sidorova", 25);
+        MyEquals humanEight = new MyEquals("Denis", "Yorsh", 39);
+
+        System.out.println("humanFive.hashCode() - " + humanFive.hashCode());
+        System.out.println("humanSix.hashCode() - " + humanSix.hashCode());
+        System.out.println("humanSeven.hashCode() - " + humanSeven.hashCode());
+        System.out.println("humanEight.hashCode() - " + humanEight.hashCode());
+        System.out.println("humanFive.equals(humanEight) - " + humanFive.equals(humanEight));
+        System.out.println("humanFive.equals(humanEight) - " + humanFive.equals(humanSix));
     }
 }
