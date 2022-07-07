70bcdc96406887ea1a4c3896416c3d641a972de1
Denis Yorsh <denis_my_mail@yahoo.com>
2022-07-07 12:28:18 +0300

    implements public class MethodsCrazyMenu protected static HashSet<String> CrazyMenu()

--- src/main/java/com/hillel/hwYorsh4/CrazyMenu.java
+++ src/main/java/com/hillel/hwYorsh4/CrazyMenu.java
@@ -4,5 +4,6 @@ public class CrazyMenu {
 
     public static void main(String[] args) {
 
+        System.out.println(MethodsCrazyMenu.CrazyMenu());
     }
 }
---
+++ src/main/java/com/hillel/hwYorsh4/MethodsCrazyMenu.java
@@ -0,0 +1,20 @@
+package com.hillel.hwYorsh4;
+
+import java.util.HashSet;
+
+public class MethodsCrazyMenu {
+
+    protected static HashSet<String> CrazyMenu() {
+        HashSet<String> crazyMenu = new HashSet<>();
+        crazyMenu.add("1 - Borscht");
+        crazyMenu.add("2 - Soup");
+        crazyMenu.add("3 - Potato");
+        crazyMenu.add("4 - Steak");
+        crazyMenu.add("5 - Salad");
+        crazyMenu.add("6 - Tea");
+        crazyMenu.add("7 - Coffee");
+        crazyMenu.add("8 - Cola");
+        crazyMenu.add("9 - Water");
+        return crazyMenu;
+    }
+}
