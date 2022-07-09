4122198a9d0020c161ab93e7ae025891ca7e53cd
Denis Yorsh <denis_my_mail@yahoo.com>
2022-07-09 13:32:06 +0300

    project Collection Cats 1.0.3

--- src/main/java/com/hillel/hwYorsh5/CollectionCats/CatNewAddress.java
+++ src/main/java/com/hillel/hwYorsh5/CollectionCats/CatNewAddress.java
@@ -7,17 +7,22 @@ public class CatNewAddress {
 
     public static void main(String[] args) {
         // New Cat List
-        List<Cat> catList = new ArrayList<>();
-        catList.add(new Kisa("Kisa", 2, "New York"));
-        catList.add(new MayMay("May May", 1, "London"));
-        catList.add(new Myrka("Myrka", 3, "Paris"));
-
+        List<Cat> catList = getCats();
         System.out.println("Old address" + catList);
 
         // Cat List pass to function moving
         List<Cat> newCatList = moving(catList);
         System.out.println("New address = " + newCatList);
     }
+
+    private static List<Cat> getCats() {
+        List<Cat> catList = new ArrayList<>();
+        catList.add(new Kisa("Kisa", 2, "New York"));
+        catList.add(new MayMay("May May", 1, "London"));
+        catList.add(new Myrka("Myrka", 3, "Paris"));
+        return catList;
+    }
+
     // We change the address
     public static List<Cat> moving(List<Cat> catList){
         // loop through on catList calling a method setAddress
