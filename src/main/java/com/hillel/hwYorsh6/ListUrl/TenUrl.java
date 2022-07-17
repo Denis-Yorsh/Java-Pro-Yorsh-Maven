a3f3248ce1a1bf05e3de86bcedec0902dd2f0c63
Denis Yorsh <denis_my_mail@yahoo.com>
2022-07-17 18:08:28 +0300

    i have domain

--- src/main/java/com/hillel/hwYorsh6/ListUrl/TenUrl.java
+++ src/main/java/com/hillel/hwYorsh6/ListUrl/TenUrl.java
@@ -2,15 +2,59 @@ package com.hillel.hwYorsh6.ListUrl;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+/**
+ *
+ * public static boolean isMoneyName(String name) {
+ *         Pattern pattern = Pattern.compile("[0-9]{2}");
+ *         Pattern pattern1 = Pattern.compile("[0-9]{1}");
+ *         Matcher matcher = pattern.matcher(name);
+ *         Matcher matcher1 = pattern1.matcher(name);
+ *         if (matcher.find()) { return false; }
+ *         if (matcher1.find()) { return true; }
+ *         return false;
+ *     }
+ *
+ */
 public class TenUrl {
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        List<String> fileToList = fileToList();
+        List<String> fileToListFirst = fileToList();
+        //List<String> fileToListSecond = fileToList();
+        System.out.println(fileToListFirst.size());
+        List<String> stringList = new ArrayList<>();
+        Pattern pattern = Pattern.compile("\\.[a-z]{2,4}\\/");
+
+        for (String string : fileToListFirst) {
+            Matcher matcher = pattern.matcher(string);
+            if (matcher.find()) {
+                stringList.add(string);
+            }
+        }
+        System.out.println(stringList.size());
+//        for (String s : stringList) {
+//            System.out.println(s);
+//        }
+
+        Set<String> domain = new HashSet<>();
+//        Map<String, Integer> stringIntegerMap = new HashMap<>();
+//        int count = 0;
+        String[] array;
+        String[] arrayTwo;
+        for (int i = 0; i < fileToListFirst.size(); i++) {
+            array = fileToListFirst.get(i).split("/");
+            arrayTwo = array[0].split("\\.");
+            domain.add(arrayTwo[arrayTwo.length-1]);
+             }
+//        for (String doman : domain) {
+//            System.out.println(doman);
+//        }
+        System.out.println(domain.size());
+
     }
 
     private static List<String> fileToList() throws FileNotFoundException {
